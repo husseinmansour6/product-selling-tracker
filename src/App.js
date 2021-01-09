b1a815b6a2f51bc7bfa8aa4d9ca4b71ac43ff8c9
husseinmansour6 <hmansour@aldogroup.com>
2021-01-09 15:59:58 -0500

    preparing the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
 import "./App.css"
 
 function App() {
-  return <div className="App">Hello world</div>
+  return (
+    <div>
+      <h1>Product Selling Tracker</h1>
+      <button>Add Product</button>
+      <table>
+        <thead>
+          <th>Product name</th>
+          <th>Quantity</th>
+          <th>Total Cost</th>
+          <th>Extra fees</th>
+          <th>Cost / item</th>
+          <th>Selling price / item</th>
+          <th>Minimum expected profit</th>
+          <th>Extra profit</th>
+          <th>Sold Quantity</th>
+          <th>Profit till date</th>
+          <th>Controls</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>product 1</td>
+            <td>20</td>
+            <td>200</td>
+            <td>0</td>
+            <td>10</td>
+            <td>15</td>
+            <td>300</td>
+            <td>0</td>
+            <td>2</td>
+            <td>30</td>
+            <td>
+              <button>Edit</button>
+              <button>Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default App
